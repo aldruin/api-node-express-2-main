5c4216a39a092a602051d6ba65b41afb0e14cf74
aldruin <abl.souza97@gmail.com>
2024-12-31 18:13:31 -0300

    ordenação

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -7,16 +7,21 @@ class LivroController {
 
   static listarLivros = async (req, res, next) => {
     try {
-      let { limite = 5, pagina = 1 } = req.query;
+      let { limite = 5, pagina = 1, ordenacao = "_id:-1" } = req.query;
 
 
+      //separando a string ordenacao por : para que identifique as duas posições, _id e -1
+      let [campoOrdenacao, ordem] = ordenacao.split(":");
       //garantindo que valores sejam numeros
       limite = parseInt(limite);
       pagina = parseInt(pagina);
+      ordem = parseInt(ordem);
 
       if (limite > 0 && pagina > 0) {
         const livrosResultado = await livros.find()
+          //sort para ordenação
           //skip para paginação, formula matematica numero da pagina -1 * limite, ex se for pagina 2, 2-1 = 1 * 5 = pular 5 livros e ir pagina 2
+          .sort({ [campoOrdenacao]: ordem })
           .skip((pagina - 1) * limite)
           .limit(limite)
           .populate("autor")
